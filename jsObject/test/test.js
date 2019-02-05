4eb0c30211be550c514c77c933296d9acec0058e
Marcus Johnson <marcus.johnson3222@gmail.com>
2019-02-05 12:56:44 -0700

    Updated test to use multiple files

--- jsObject/test/test.js
+++ jsObject/test/test.js
@@ -1,9 +1,9 @@
-card = require('./card.js');
+card = require('../card.js');
 assert = require('assert');
 
 describe('Array', function() {
 	describe('#constructor()', function() {
-		it('should have its constructed name', function(){
+		it('bearCard should have its constructed name be bear', function(){
 			let bearCard = new card.Card('Bear','monster')
 			assert.equal(bearCard.name,'Bear');
 		});
