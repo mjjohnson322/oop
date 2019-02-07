23ce09d8b4334ce4d8780e188f12a1c077221e34
Marcus Johnson <Marcus.Johnson3222@gmail.com>
2019-02-07 12:29:41 -0700

    Added test for setting attack

--- jsObject/test/cardTest.js
+++ jsObject/test/cardTest.js
@@ -3,9 +3,20 @@ assert = require('assert');
 
 describe('Array', function() {
 	describe('#constructor()', function() {
-		it('bearCard should have its constructed name be bear', function(){
+		it('should have its constructed name be bear', function(){
 			let bearCard = new card.Card('Bear','monster')
 			assert.equal(bearCard.name,'Bear');
 		});
+	});
+	describe('#setAtk()', function(){
+		it('should have 1000 attack', function(){
+			let bearCard = new card.Card('Bear','monster');
+			bearCard.atk = 1000;
+			assert.equal(bearCard.atk=1000);
+		});
+		it('should throw an exception when atk is not a valid number', function(){
+			let bearCard = new card.Card('Bear','monster');
+			//bearCard.atk = 'String';
+		})
 	});
 });
