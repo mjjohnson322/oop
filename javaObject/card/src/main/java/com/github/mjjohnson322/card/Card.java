3c4767c5ab336167662dcc40d63397a2b3e0cdbe
Marcus Johnson <Marcus.Johnson3222@gmail.com>
2019-02-18 18:22:00 -0700

    Added getters and setters for Card

--- javaObject/card/src/main/java/com/github/mjjohnson322/card/Card.java
+++ javaObject/card/src/main/java/com/github/mjjohnson322/card/Card.java
@@ -10,18 +10,33 @@ package com.github.mjjohnson322.card;
  * @author Marcus
  */
 public class Card {
-    String cardName;
-    String cardType;
-    int atk;
-    int def;
-    Effects effects = new Effects();
+    private String cardName;
+    private String cardType;
+    private int atk;
+    private int def;
+    private Effects effects = new Effects();
     Card(String name, String type){
         cardName=name;
         cardType=type;
         atk=0;
         def=0;
     }
-    void addEffect(String effect){
+    public void addEffect(String effect){
         effects.addEffect(effect);
+    }
+    public void setAtk(int _atk){
+        atk=_atk;
+    }
+    public void setDef(int _def){
+        def=_def;
+    }
+    public int getAtk(){
+        return atk;
+    }
+    public int getDef(){
+        return def;
+    }
+    public Effects getEffects(){
+        return effects;
     }
 }
