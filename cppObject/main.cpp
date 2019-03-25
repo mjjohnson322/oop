a0aff308ede9afd59c5334769b25866873a5ee9a
Marcus Johnson <Marcus.Johnson3222@gmail.com>
2019-03-24 18:49:48 -0600

    Added main and a makefile

---
+++ cppObject/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include "player.h"
+
+using namespace player;
+
+void testGetName(Player player1){
+    assert(player1.getName()=="Name");
+}
+void testGetClass(Player player1){
+    assert(player1.getClass()=="Wizard");
+}
+void testGetLevel(Player player1){
+    assert(player1.getLevel()==1);
+}
+void testSetLevel(Player player1){
+    player1.setLevel(10);
+    assert(player1.getLevel()==10);
+}
+void testLevelUp(Player player1){
+    player1.levelUp();
+    assert(player1.getLevel()==2);
+}
+
+void test(Player player1){
+    testGetLevel(player1);
+    testGetClass(player1);
+    testGetName(player1);
+    testSetLevel(player1);
+    testLevelUp(player1);
+}
+
+
+int main(int argc, char **argv){
+    Player player1 = new Player("Name", "Wizard", 1);
+    test(player1);
+    return 0;
+}
