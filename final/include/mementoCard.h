dcbc7eb9ef847c303b82b224b4eda07173c18314
Marcus Johnson <Marcus.Johnson3222@gmail.com>
2019-05-15 19:40:44 -0600

    Whoops forgot a spot in fix

--- final/include/mementoCard.h
+++ final/include/mementoCard.h
@@ -4,15 +4,15 @@
 
 namespace card{
     class MementoCard{
-        public: MementoCard(std::string _name/*, std::vector<std::string> _effects*/);
+        public: MementoCard(std::string _name, std::vector<std::string> _effects);
         private: std::string name;
-        /*private: std::vector<std::string> effects;*/
+        private: std::vector<std::string> effects;
         public: std::string getName();
         public: std::vector<std::string> getEffects();
     };
     class MementoMonsterCard : public MementoCard {
         public: MementoMonsterCard(std::string _name, int _atk, int _def, 
-            int _level/*, std::vector<std::string> _effects*/);
+            int _level, std::vector<std::string> _effects);
         private: int atk;
         private: int def;
         private: int level;
@@ -21,14 +21,14 @@ namespace card{
         public: int getLevel();
     };
     class MementoSpellCard : public MementoCard {
-        public: MementoSpellCard(std::string _name, SpellType _type/*, 
-            std::vector<std::string> _effects*/);
+        public: MementoSpellCard(std::string _name, SpellType _type,
+            std::vector<std::string> _effects);
         private: SpellType type;
         public: SpellType getSpellType();
     };
     class MementoPendCard : public MementoMonsterCard, public MementoSpellCard {
         public: MementoPendCard(std::string _name, int _atk, int _def, 
-            int _scale, int level/*, std::vector<std::string> _effects*/);
+            int _scale, int level, std::vector<std::string> _effects);
         private: int scale;
         public: int getScale();
     };
