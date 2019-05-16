1cb65970bb53fb22d0641e795ab4e6c5724bcaa0
Marcus Johnson <Marcus.Johnson3222@gmail.com>
2019-05-15 18:00:54 -0600

    Bug fixes

--- final/include/mementoCard.h
+++ final/include/mementoCard.h
@@ -8,22 +8,22 @@ namespace card{
         public: MementoCard(std::string _name, std::vector<std::string> _effects);
         private: std::string name;
         private: std::vector<std::string> effects;
-    }
+    };
     class MementoMonsterCard : public MementoCard {
         public: MementoMonsterCard(std::string _name, int _atk, int _def, 
             int _level, std::vector<std::string> _effects);
         private: int def;
         private: int atk;
         private: int level;
-    }
+    };
     class MementoSpellCard : public MementoCard {
         public: MementoSpellCard(std::string _name, SpellType _type, 
             std::vector<std::string> _effects);
         private: SpellType type;
-    }
+    };
     class MementoPendCard : public MementoMonsterCard, public MementoSpellCard {
         public: MementoPendCard(std::string _name, int _atk, int _def, 
             int _scale, int level, std::vector<std::string> _effects);
         private: int scale;
-    }
+    };
 }
