5593683db8ffd5948338c9024c48aa82c9744669
Marcus Johnson <Marcus.Johnson3222@gmail.com>
2019-05-15 20:27:25 -0600

    Why do I have two diamond of deaths this was a poor decision

--- final/include/mementoCard.h
+++ final/include/mementoCard.h
@@ -7,8 +7,8 @@ namespace card{
         public: MementoCard(std::string _name, std::vector<std::string> _effects);
         private: std::string name;
         private: std::vector<std::string> effects;
-        public: virtual std::string getName();
-        public: virtual std::vector<std::string> getEffects();
+        public: virtual std::string getName() const;
+        public: virtual std::vector<std::string> getEffects() const;
         public: virtual ~MementoCard();
     };
     class MementoMonsterCard : public MementoCard {
@@ -17,23 +17,23 @@ namespace card{
         private: int atk;
         private: int def;
         private: int level;
-        public: int getAtk();
-        public: int getDef();
-        public: int getLevel();
+        public: int getAtk() const;
+        public: int getDef() const;
+        public: int getLevel() const;
         public: ~MementoMonsterCard();
     };
     class MementoSpellCard : public MementoCard {
         public: MementoSpellCard(std::string _name, SpellType _type,
             std::vector<std::string> _effects);
         private: SpellType type;
-        public: SpellType getSpellType();
+        public: SpellType getSpellType() cosnt;
         public: ~MementoSpellCard();
     };
     class MementoPendCard : public MementoMonsterCard, public MementoSpellCard {
         public: MementoPendCard(std::string _name, int _atk, int _def, 
             int _scale, int level, std::vector<std::string> _effects);
         private: int scale;
-        public: int getScale();
+        public: int getScale() const;
         public: ~MementoPendCard();
     };
 }
